0222aa854f8ee23095673907d342f22d808702f8
Imadnajam <imad00naj11@gmail.com>
2023-11-24 13:48:06 +0100

    Axios And Fetch API

---
+++ src/components/AXIOS.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+export default function AXIOS() {
+  const [users, setUsers] = useState([]);
+  const [todo, setTodo] = useState([]);
+  const [listT, setListT] = useState([]);
+
+  function AfficheU() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((don) => setUsers(don));
+  }
+
+  function AfficheT(e) {
+    var id = e.target.value;
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then((response) => response.json())
+      .then((don) => setTodo([don]));
+  }
+
+  function AfficheLT() {
+    fetch(`https://jsonplaceholder.typicode.com/todos`)
+      .then((response) => response.json())
+      .then((don) => setListT(don)); // Correction ici
+  }
+
+  return (
+    <div>
+      <button onClick={AfficheU}>
+        <h3>clic ici pour afficher La liste des users</h3>
+      </button>{' '}
+      <br />
+
+      {users.map((e, i) => (
+        <li key={i}>
+          {' '}
+          <span style={{ color: 'red' }}>le Nom :</span> {e.name}{' '}
+          <span style={{ color: 'teal' }}>le Prenom :</span> {e.username}{' '}
+          <button value={e.id} onClick={AfficheT} style={{ color: 'green' }}>
+            Affiche les todo
+          </button>
+          {todo.length > 0 && e.id === todo[0].userId && (
+            <ul>
+              {todo.map((t, j) => (
+                <li key={j}>
+                  <span style={{ color: 'blue' }}>Todo :</span> {t.title}
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+      ))}
+
+      <button onClick={AfficheLT}>
+        <h3>clic ici pour afficher La liste des Tache</h3>
+      </button>{' '}
+      {listT.map((e, i) => (
+        <li key={i}>
+          {' '}
+          <span style={{ color: 'red' }}>le Nom :</span> {e.title}
+        </li>
+      ))}
+    </div>
+  );
+}
